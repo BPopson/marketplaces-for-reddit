5493990b6b487a56af234c06d5e490e1cf413a52
Brandon Popson <BPopson@users.noreply.github.com>
2019-08-31 16:02:24 -0500

    Sets up basic home index page

--- marketplaces_for_reddit/marketplaces_for_reddit/urls.py
+++ marketplaces_for_reddit/marketplaces_for_reddit/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('reddit/', include('MarketplacesForReddit.urls')),
+    path('', include('MarketplacesForReddit.urls')),
     path('admin/', admin.site.urls),
 ]
