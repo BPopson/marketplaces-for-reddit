d298f214edc5af1e3bae16cf7d98a839cc644a40
Brandon Popson <BPopson@users.noreply.github.com>
2019-09-10 21:00:22 -0500

    Adds extra information to the django admin page for listings

--- marketplaces_for_reddit/MarketplacesForReddit/admin.py
+++ marketplaces_for_reddit/MarketplacesForReddit/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 
 from .models import Listing
 
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_utc', 'link_flair_text', 'subreddit_name_prefixed', 'get_wants', 'get_has', 'get_location')
+    list_filter = ['link_flair_text', 'created_utc']
+    search_fields = ['title']
+
+
 # Register your models here.
-admin.site.register(Listing)
+admin.site.register(Listing, ListingAdmin)
