9afc3ac3eb0c90a61a42794b4d549214e5be4c0f
Brandon Popson <BPopson@users.noreply.github.com>
2019-09-17 21:00:13 -0500

    Sets up basic very homepage

--- marketplaces_for_reddit/MarketplacesForReddit/views.py
+++ marketplaces_for_reddit/MarketplacesForReddit/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 
-
+from .models import Listing
 # Create your views here.
 def index(request):
+    listings = Listing.objects.order_by('-created_utc')
+
     context = {
-        'test': "Hello, world. You're at the Marketplaces For Reddit index."
+        'test': "Hello, world. You're at the Marketplaces For Reddit index.",
+        'listings': listings[0:5]
     }
     return render(request, 'home/index.html', context)
