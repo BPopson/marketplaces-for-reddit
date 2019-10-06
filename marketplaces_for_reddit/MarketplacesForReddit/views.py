e26fc1a86fe66966471e12e33a1ffd02072b7854
Brandon Popson <BPopson@users.noreply.github.com>
2019-10-06 10:44:52 -0500

    Starts the addition of the search page and fix some formatting issues

--- marketplaces_for_reddit/MarketplacesForReddit/views.py
+++ marketplaces_for_reddit/MarketplacesForReddit/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render, get_list_or_404
 
 from .models import Listing, ParsedListing
+
+
 # Create your views here.
 def index(request):
     listings = Listing.objects.order_by('-created_utc')
-    parsedListings = ParsedListing.objects.values('location').distinct().order_by('location')
+    parsed_listings_locations = ParsedListing.objects.values('location').distinct().order_by('location')
 
     context = {
         'listings': listings[0:10],
-        'locations': parsedListings,
+        'locations': parsed_listings_locations,
     }
     return render(request, 'home/index.html', context)
+
+
+def search(request):
+    listings = Listing.objects.order_by('-created_utc')
+    parsed_listings_locations = ParsedListing.objects.values('location').distinct().order_by('location')
+
+    context = {
+        'listings': listings[0:10],
+        'locations': parsed_listings_locations,
+    }
+    return render(request, 'search/index.html', context)
