7a866c6cfc55bbbacb8b2a569e89a346a41d0a8b
Brandon Popson <BPopson@users.noreply.github.com>
2019-10-08 22:43:27 -0500

    Attempts to fix the widgets not rendering on the search page

--- marketplaces_for_reddit/MarketplacesForReddit/forms.py
+++ marketplaces_for_reddit/MarketplacesForReddit/forms.py
@@ -2,18 +2,24 @@ from django import forms
 
 
 class SearchForm(forms.Form):
-    search = forms.TextInput()
-    location = forms.SelectMultiple(choices=('test', 'test2', 'test3'))
-    date_within = forms.Select(choices=('1 day',
-                                        '3 days',
-                                        '1 week',
-                                        '2 weeks',
-                                        '1 month',
-                                        '2 months',
-                                        '6 months',
-                                        '1 year'))
-    date = forms.DateInput()
+    DATE_WITHIN_CHOICES = [('1', '1 day'),
+                           ('3', '3 days'),
+                           ('7', '1 week'),
+                           ('14', '2 weeks'),
+                           ('30', '1 month'),
+                           ('60', '2 months'),
+                           ('180', '6 months'),
+                           ('365', '1 year')]
+    LOCATION_CHOICES = [('USA', '[USA]'),
+                        ('CAN', '[CAN]')]
+    LISTING_CHOICES = [('selling', 'Selling'),
+                       ('buying', 'Buying'),
+                       ('trading', 'Trading')]
+    search = forms.CharField()
+    location = forms.MultipleChoiceField(choices=LOCATION_CHOICES)
+    date_within = forms.ChoiceField(choices=DATE_WITHIN_CHOICES)
+    date = forms.DateField(input_formats=['%Y-%m-%d'])
     number_of_trades = forms.NumberInput()
     number_of_trades_filter = forms.Select(choices=('greater than', 'less than'))
-    listing_type = forms.RadioSelect(choices=('Selling', 'Buying', 'Trading'))
+    listing_type = forms.ChoiceField(choices=LISTING_CHOICES, widget=forms.RadioSelect)
     payment_type = forms.CheckboxSelectMultiple(choices=('PayPal', 'Local Cash', 'Other'))
