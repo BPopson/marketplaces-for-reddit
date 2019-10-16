8089e181fb62b7490cee77bd62bb3c281c9da6c1
Brandon Popson <Brandon.Popson@outlook.com>
2019-10-16 14:06:21 -0500

    Updates the SearchForm attributes
    Use standard django form fields instead of widgets. Using widgets results in the form input not showing.

--- marketplaces_for_reddit/MarketplacesForReddit/forms.py
+++ marketplaces_for_reddit/MarketplacesForReddit/forms.py
@@ -14,12 +14,18 @@ class SearchForm(forms.Form):
                         ('CAN', '[CAN]')]
     LISTING_CHOICES = [('selling', 'Selling'),
                        ('buying', 'Buying'),
-                       ('trading', 'Trading')]
-    search = forms.CharField()
-    location = forms.MultipleChoiceField(choices=LOCATION_CHOICES)
-    date_within = forms.ChoiceField(choices=DATE_WITHIN_CHOICES)
-    date = forms.DateField(input_formats=['%Y-%m-%d'])
-    number_of_trades = forms.NumberInput()
-    number_of_trades_filter = forms.Select(choices=('greater than', 'less than'))
-    listing_type = forms.ChoiceField(choices=LISTING_CHOICES, widget=forms.RadioSelect)
-    payment_type = forms.CheckboxSelectMultiple(choices=('PayPal', 'Local Cash', 'Other'))
+                       ('trading', 'Trading'),
+                       ('closed', 'Closed')]
+    TRADE_COUNT_FILTERS = [('gt', 'Greater Than'), ('lt', 'Less Than')]
+    PAYMENT_TYPES = [('paypal', 'PayPal'), ('cash', 'Local Cash'), ('other', 'Other')]
+
+    search = forms.CharField(label='Search')
+    search_title_only = forms.BooleanField(label='Search Title Only')
+    location = forms.MultipleChoiceField(label='Locations', choices=LOCATION_CHOICES)
+    date_within = forms.ChoiceField(label='Date Within', choices=DATE_WITHIN_CHOICES)
+    date = forms.DateField(label='Date')
+    number_of_trades = forms.IntegerField()
+    number_of_trades_filter = forms.ChoiceField(choices=TRADE_COUNT_FILTERS)
+    listing_type = forms.MultipleChoiceField(label='Listing Type', choices=LISTING_CHOICES)
+    payment_type = forms.MultipleChoiceField(choices=PAYMENT_TYPES)
+
