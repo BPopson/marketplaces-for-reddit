60da5e9552e18c6f42726215330a038846b30cff
Brandon Popson <Brandon.Popson@outlook.com>
2019-10-23 14:37:11 -0500

    Cleans up views.py and adds the SearchForm

--- marketplaces_for_reddit/MarketplacesForReddit/views.py
+++ marketplaces_for_reddit/MarketplacesForReddit/views.py
@@ -1,28 +1,38 @@
 from datetime import date, datetime, timedelta
 
 from django.db.models import Q
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from .models import Listing, ParsedListing
 from .forms import SearchForm
 
 
-# Create your views here.
+# Home Page/Index View
 def index(request):
     context = {}
     listings = Listing.objects.order_by('-created_utc')
-    parsed_listings_locations = ParsedListing.objects.values('location').distinct().order_by('location')
+    parsed_listings_locations = ParsedListing.objects.values('location') \
+        .distinct().order_by('location')
+    search_form = SearchForm(initial={
+        'date': date.today().strftime('%Y-%m-%d'),
+        'number_of_trades': 0})
 
     context['listings'] = listings[0:10]
     context['locations'] = parsed_listings_locations
 
+    context['search_form'] = search_form
+
     return render(request, 'home/index.html', context)
 
-
+# Search View
 def search(request):
     context = {}
 
+    search_form = SearchForm(request.GET)
+
     context['query_param_search'] = request.GET.get('search', '')
+    context['query_param_search_title_only'] = request.GET.get('search_title_only', False)
     context['query_param_location'] = request.GET.get('location', 'USA')
     context['query_param_date'] = request.GET.get('date', date.today().strftime('%Y-%m-%d'))
     context['query_param_date_within'] = request.GET.get('date_within', '7')
@@ -30,7 +40,6 @@ def search(request):
     context['query_param_trade_sort'] = request.GET.get('trade_sort', 'gt')
     context['query_param_listing_type'] = request.GET.get('listing_type', 'selling')
     context['query_param_payment_types'] = request.GET.get('payment_types', 'paypal')
-
 
     search_params = {
         'search': context['query_param_search'],
@@ -41,8 +50,8 @@ def search(request):
         'date_within': int(context['query_param_date_within']),
     }
 
-
-    parsed_listings_locations = ParsedListing.objects.values('location').distinct().order_by('location')
+    parsed_listings_locations = ParsedListing.objects.values('location') \
+        .distinct().order_by('location')
     listings = Listing.objects \
                     .filter(Q(title__icontains=search_params['search']) \
                             | Q(selftext__icontains=search_params['search'])) \
@@ -54,7 +63,7 @@ def search(request):
                     .filter(title__icontains=search_params['location']) \
                     .order_by('-created_utc')
 
-    context['search_form'] = SearchForm()
+    context['search_form'] = search_form
     context['listings'] = listings[0:25]
     context['locations'] = parsed_listings_locations
 
