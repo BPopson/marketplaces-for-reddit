2ed61d6af74681c878ccc4a2694ac6e4b4daca43
Brandon Popson <Brandon.Popson@outlook.com>
2019-11-06 13:33:21 -0600

    Changes the database settings to use postgres

--- marketplaces_for_reddit/marketplaces_for_reddit/settings.py
+++ marketplaces_for_reddit/marketplaces_for_reddit/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'marketplaces_for_reddit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['POSTGRES_NAME'],
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        'HOST': os.environ['POSTGRES_HOST'],
+        'PORT': os.environ['POSTGRES_PORT'],
     }
 }
 
