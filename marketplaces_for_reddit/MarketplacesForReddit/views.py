c101769a724ef7ee10b4a635a13af067ccf73951
Brandon Popson <Brandon.Popson@outlook.com>
2019-11-20 14:04:21 -0600

    Removes old code

--- marketplaces_for_reddit/MarketplacesForReddit/views.py
+++ marketplaces_for_reddit/MarketplacesForReddit/views.py
@@ -40,43 +40,6 @@ def search(request):
         request.session.create()
 
     search_form = SearchForm(request.GET)
-
-    # context['query_param_search'] = request.GET.get('search', '')
-    # context['query_param_search_title_only'] = request.GET.get('search_title_only', False)
-    # context['query_param_location'] = request.GET.get('location', 'USA')
-    # context['query_param_date'] = request.GET.get('date', date.today().strftime('%Y-%m-%d'))
-    # context['query_param_date_within'] = request.GET.get('date_within', '7')
-    # context['query_param_trade_amount'] = request.GET.get('trade_amount', '1')
-    # context['query_param_trade_sort'] = request.GET.get('trade_sort', 'gt')
-    # context['query_param_listing_type'] = request.GET.get('listing_type', 'selling')
-    # context['query_param_payment_types'] = request.GET.get('payment_types', 'paypal')
-
-    # search_params = {
-    #     'search': context['query_param_search'],
-    #     'location': context['query_param_location'],
-    #     'listing_type': context['query_param_listing_type'],
-    #     'payment_types': context['query_param_payment_types'],
-    #     'date': datetime.strptime(context['query_param_date'], '%Y-%m-%d'),
-    #     'date_within': int(context['query_param_date_within']),
-    # }
-
-    # parsed_listings_locations = ParsedListing.objects.values('location') \
-    #     .distinct().order_by('location')
-    # listings = Listing.objects \
-    #                 .filter(Q(title__icontains=search_params['search']) \
-    #                         | Q(selftext__icontains=search_params['search'])) \
-    #                 .filter(link_flair_text__icontains=search_params['listing_type']) \
-    #                 .filter(title__icontains=search_params['payment_types']) \
-    #                 .filter(created_utc__gte=search_params['date'], \
-    #                         created_utc__lt=search_params['date'].date()
-    #                         + timedelta(days=search_params['date_within'])) \
-    #                 .filter(title__icontains=search_params['location']) \
-    #                 .order_by('-created_utc')
-
-    # context['search_form'] = search_form
-    # context['listings'] = listings[0:25]
-    # context['locations'] = parsed_listings_locations
-
     search_params = {}
 
     if search_form.is_valid():
@@ -121,6 +84,7 @@ def search(request):
                         (Q(title__icontains=x) for x in search_params['location']))) \
                     .filter(created_utc__gte=search_params['date'] - timedelta(days=search_params['date_within']), \
                             created_utc__lte=search_params['date'])
+
     if search_params['search']:
         if search_params['search_title_only']:
             vector = SearchVector('title')
@@ -133,18 +97,11 @@ def search(request):
             listings = listings.annotate(search=vector).filter(search=query)
             # listings = listings.filter(Q(title__icontains=search_params['search']) \
             #                           | Q(selftext__icontains=search_params['search']))
-        
-
-    # if search_params['number_of_trades_filter'] == 'gt'
-    #     listings = listings.filter()
-    # elif:
-
 
     listings = listings.order_by('-created_utc')
 
     context['SEARCH_PARAMS'] = search_params
     context['search_form'] = search_form
     context['listings'] = listings[0:25]
-    # context['locations'] = parsed_listings_locations
 
     return render(request, 'search/index.html', context)
